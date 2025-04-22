3ae3ad4d0f68e9ae9b6ab2c71870621de18a9212
Mayank Jangid <226301131@gkv.ac.in>
2025-04-22 19:59:36 +0530

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Mayank Jangid on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
